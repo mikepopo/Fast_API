d571b667ac4691693ba4c0c72411c36c4f7125d1
mh <2831990485@qq.com>
2026-09-22 11:22:45 +0800

    请求与响应

--- main.py
+++ main.py
@@ -30,3 +30,34 @@ class User(BaseModel):
 @app.post("/users")
 async def user(user:User):
     return user
+
+
+##### 请求与响应 ######
+#1.装饰器中指定响应类
+from fastapi.responses import HTMLResponse
+@app.get("/html",response_class=HTMLResponse)
+async def book():
+    return "<H1>这是一级标题</H1>"
+
+#2.return 中返回响应对象
+from fastapi.responses import FileResponse
+@app.get("/sponse")
+async def book1():
+    return FileResponse("./File/Snipaste_2026-09-22_10-44-00.png")
+
+#3.自定义类响应数据格式
+from pydantic import BaseModel
+class News(BaseModel):
+    id:str
+    title:str
+    content:str
+
+@app.get("/books{id}",response_model=News)
+async def books(id:str):
+    return {
+        "id":f"该书的编号为{id}",
+        "title":"余华",
+        "content":"这是一本好书"
+
+    }
+
